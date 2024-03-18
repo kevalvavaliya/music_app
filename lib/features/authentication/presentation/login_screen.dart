3b51123f491ed5715aa0355d14de77c7a800da8b
kevalvavaliya <kevalvavaliya@gmail.com>
2024-03-18 19:15:19 +0530

    intialize project and configured firebase

---
+++ lib/features/authentication/presentation/login_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+  static const String routeName = '/login';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Loginscreen"),
+      ),
+    );
+  }
+}
